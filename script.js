64e613ca9ffc6074c442bc1b32890bed3aee1417
Fernando Adolfo <67721157+Ferthronix224@users.noreply.github.com>
2024-12-22 10:38:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const countdownElement = document.getElementById('countdown');
 
 function updateCountdown() {
     const current_date = new Date()
-    const selected_date = new new Date(current_date.getFullYear(), 2, 22)
+    const selected_date = new Date(current_date.getFullYear(), 2, 22)
 
     const current_date_str = current_date.toDateString()
     const selected_date_str = selected_date.toDateString()
